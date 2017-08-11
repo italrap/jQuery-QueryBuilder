e1bc598e0ec1ac7db569aaf1d5a4415bc85e1acb
Damien SOREL <damien.sorel@sogelink.fr>
2017-08-11 14:41:36 +0200

    WIP: Fix #544 Always convert int, float and bool values

--- src/data.js
+++ src/data.js
@@ -394,11 +394,20 @@ QueryBuilder.prototype.getRuleInputValue = function(rule) {
             }
         }
 
-        if (operator.multiple && filter.value_separator) {
-            value = value.map(function(val) {
-                return val.split(filter.value_separator);
-            });
-        }
+        value = value.map(function(val) {
+            if (operator.multiple && filter.value_separator && typeof val == 'string') {
+                val = val.split(filter.value_separator);
+            }
+
+            if ($.isArray(val)) {
+                return val.map(function(subval) {
+                    return Utils.changeType(subval, filter.type);
+                });
+            }
+            else {
+                return Utils.changeType(val, filter.type);
+            }
+        });
 
         if (operator.nb_inputs === 1) {
             value = value[0];
